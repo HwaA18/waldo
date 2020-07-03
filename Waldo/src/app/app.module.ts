f8246bbd0cfbef3050c7f12fd1324277601bf0f4
hwaa18 <austinhwa@MacBook-Pro.fios-router.home>
2020-07-03 18:20:14 -0400

    Fixed the "For Development Only" Issue on the Maps Page

--- Waldo/src/app/app.module.ts
+++ Waldo/src/app/app.module.ts
@@ -12,6 +12,10 @@ import { AppComponent } from './app.component';
 //added the import statements for Google Maps
 import { GoogleMaps } from '@ionic-native/google-maps';
 
+//new imports for google maps
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -20,7 +24,9 @@ import { GoogleMaps } from '@ionic-native/google-maps';
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    GoogleMaps //Added the google maps into the NgModule
+    GoogleMaps,  //Added the google maps into the NgModule
+    Geolocation,
+    NativeGeocoder
   ],
   bootstrap: [AppComponent]
 })
